4116474f8e356fc2036be3124af88811ac78990f
IARA <IARAMB>
2023-11-19 18:33:14 -0300

    Minha terceira commit

---
+++ Criadouro/src/criadouro/Animalvoadores.java
@@ -0,0 +1,17 @@
+package criadouro;
+
+public class Animalvoadores {
+	
+	String tipos;
+	String genero;
+	
+	void infoaves() {
+		System.out.println("Tipos: " + tipos);
+		System.out.println("Gênero disponível: " + genero);
+	}
+	
+	void infoesp() {
+		System.out.println("As aves são protegidas pelo IBAMA.");
+	}
+
+}
---
+++ Criadouro/src/criadouro/Infoanimais.java
@@ -0,0 +1,16 @@
+package criadouro;
+
+public class Infoanimais {
+
+	public static void main(String[] args) {
+		Animalvoadores animalvoadores = new Animalvoadores();
+		
+		animalvoadores.tipos = "Aves silvestres";
+		animalvoadores.genero = "Fêmeas";
+		
+		animalvoadores.infoaves();
+		animalvoadores.infoesp();
+
+	}
+
+}
